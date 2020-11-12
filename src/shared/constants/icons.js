3938c0e5debec70e8ea87575413cf423c2965c4a
Aditi Singh <aditi.singh@chicmic.co.in>
2020-11-12 16:12:18 +0530

    group icon added in watchparty list

--- src/shared/constants/icons.js
+++ src/shared/constants/icons.js
@@ -1,3 +1,4 @@
-export const CLOSE_ICON = require(`../../assets/img/icons/close_icon.svg`)
+export const CLOSE_ICON = require(`../../assets/img/icons/close_icon.svg`);
+export const GROUP_ICON = require(`../../assets/img/icons/group.png`)
 
 
